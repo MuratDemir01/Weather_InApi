253b8ecae65e7bd2d7e3950185ce5a35805f9335
MuratDemir01 <muratram96@hotmail.com>
2018-02-18 07:24:28 +0300

    send

---
+++ Weather/Controllers/AnaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Weather.Controllers
+{
+    public class AnaController : Controller
+    {
+        // GET: Ana
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+    }
+}
